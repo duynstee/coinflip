915c8ddb3e27cfa2446cd79caeb0ba50d0c3c1c5
duynstee <74192436+duynstee@users.noreply.github.com>
2022-03-25 18:49:08 +0100

    api werkend begin

--- backend/aspnetserverTutorial/aspnetserver/Program.cs
+++ backend/aspnetserverTutorial/aspnetserver/Program.cs
@@ -3,6 +3,18 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CORSPolicy", 
+        builder =>
+        {
+            builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000", "https://appname.azurestaticapps.net");
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,10 +30,12 @@ app.UseSwaggerUI(swaggerUIOptions =>
 {
     swaggerUIOptions.DocumentTitle = "ASP.NET React Tutorial";
     swaggerUIOptions.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API serving a very simple Post model");
-    swaggerUIOptions.RoutePrefix = String.Empty;
+    swaggerUIOptions.RoutePrefix = string.Empty;
 });
 
 app.UseHttpsRedirection();
+
+app.UseCors("CORSPolicy");
 
 app.MapGet("/get-all-posts", async () => await PostsRepository.GetPostsAsync())
     .WithTags("Posts Endpoints");
@@ -30,7 +44,7 @@ app.MapGet("/get-post-by-id/{postId}", async (int postId) =>
 {
     Post postToReturn = await PostsRepository.GetPostByIdAsync(postId);
 
-    if (postToReturn == null)
+    if (postToReturn != null)
     {
         return Results.Ok(postToReturn);
     }
