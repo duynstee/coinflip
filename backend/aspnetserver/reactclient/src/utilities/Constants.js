7eca2873a2609c6efa47f82c1737dbf7fa25eada
duynstee <74192436+duynstee@users.noreply.github.com>
2022-03-29 08:38:58 +0200

    constants

---
+++ backend/aspnetserver/reactclient/src/utilities/Constants.js
@@ -0,0 +1,18 @@
+const API_BASE_URL_DEVELOPMENT = "https://localhost:7273";
+const API_BASE_URL_PRODUCTION = "https://appname.azurewebsites.net";
+
+const ENDPOINT = {
+  GET_ALL_POSTS: "get-all-posts",
+  GET_POST_BY_ID: "get-post-by-id",
+  CREATE_POST: "create-post",
+  UPDATE_POST: "update-post",
+  DELETE_POST_BY_ID: "delete-post-by-id",
+};
+
+const development = {
+  API_URL_GET_ALL_POSTS: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINT.GET_ALL_POSTS}}`,
+  API_URL_GET_POST_BY_ID: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINT.GET_POST_BY_ID}}`,
+  API_URL_CREATE: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINT.CREATE_POST}}`,
+  API_URL_UPDATE_POST: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINT.UPDATE_POST}}`,
+  API_URL_DELETE_POST_BY_ID: `${API_BASE_URL_DEVELOPMENT}/${ENDPOINT.DELETE_POST_BY_ID}}`,
+};
